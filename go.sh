fc615f365ca7e51f02d74d62bc06f49ec4427c29
Lori Corbani <lori.corbani@jax.org>
2016-06-08 13:14:00 -0400

    TR12049/gone-dor & more

--- go.sh
+++ go.sh
@@ -2,6 +2,8 @@
 #
 #  go.sh
 #
+#  See http://prodwww.informatics.jax.org/wiki/index.php/sw:Goload_overview
+#
 #  Run all GOA loads from loadadmin/sundaytasks.csh
 #
 
@@ -21,19 +23,19 @@ touch $LOG
  
 date | tee -a $LOG
 
-echo 'Run GOA Load' | tee -a ${LOG}
+echo '1:Run GOA/Mouse Load' | tee -a ${LOG}
 ${GOLOAD}/goamouse/goamouse.sh | tee -a ${LOG}
 
-echo 'Run GO/Rat Load' | tee -a ${LOG}
+echo '2:Run GOA/Rat Load' | tee -a ${LOG}
 ${GOLOAD}/goarat/goarat.sh | tee -a ${LOG}
 
-echo 'Run GO/PAINT Load' | tee -a ${LOG}
+echo '3:Run GOA/PAINT Load' | tee -a ${LOG}
 ${GOLOAD}/goarefgen/goarefgen.sh | tee -a ${LOG}
 
-echo 'Run GOA/Human Load' | tee -a ${LOG}
+echo '4:Run GOA/Human Load' | tee -a ${LOG}
 ${GOLOAD}/goahuman/goahuman.sh | tee -a ${LOG}
 
-echo 'Run GO/CFP Load' | tee -a ${LOG}
+echo '5:Run GOA/CFP Load' | tee -a ${LOG}
 ${GOLOAD}/goacfp/goacfp.sh | tee -a ${LOG}
 
 date | tee -a $LOG
