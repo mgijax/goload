d8c8c9c09562ee9a15dbc2c91d79b123f72af5a4
Lori Corbani <lori.corbani@jax.org>
2016-06-14 07:27:30 -0400

    TR12049/gone-dor & more

--- go.sh
+++ go.sh
@@ -17,28 +17,28 @@ fi
 
 . ${MGICONFIG}/master.config.sh
 
-LOG=${DATALOADSOUTPUT}/go/$0.log
-rm -rf $LOG
-touch $LOG
+GOLOG=${DATALOADSOUTPUT}/go/goload.log
+rm -rf $GOLOG
+touch $GOLOG
  
-date | tee -a $LOG
+date | tee -a $GOLOG
 
-#echo '1A:Run GOA/Mouse/Noctua Load' | tee -a ${LOG}
-#${GOLOAD}/goamousenoctua/goamousenoctua.sh | tee -a ${LOG}
+#echo '1A:Run GOA/Mouse/Noctua Load' | tee -a ${GOLOG}
+#${GOLOAD}/goamousenoctua/goamousenoctua.sh | tee -a ${GOLOG}
 
-echo '1:Run GOA/Mouse Load' | tee -a ${LOG}
-${GOLOAD}/goamouse/goamouse.sh | tee -a ${LOG}
+echo '1:Run GOA/Mouse Load' | tee -a ${GOLOG}
+${GOLOAD}/goamouse/goamouse.sh | tee -a ${GOLOG}
 
-echo '2:Run GOA/Rat Load' | tee -a ${LOG}
-${GOLOAD}/goarat/goarat.sh | tee -a ${LOG}
+echo '2:Run GOA/Rat Load' | tee -a ${GOLOG}
+${GOLOAD}/goarat/goarat.sh | tee -a ${GOLOG}
 
-echo '3:Run GOA/PAINT Load' | tee -a ${LOG}
-${GOLOAD}/goarefgen/goarefgen.sh | tee -a ${LOG}
+echo '3:Run GOA/PAINT Load' | tee -a ${GOLOG}
+${GOLOAD}/goarefgen/goarefgen.sh | tee -a ${GOLOG}
 
-echo '4:Run GOA/Human Load' | tee -a ${LOG}
-${GOLOAD}/goahuman/goahuman.sh | tee -a ${LOG}
+echo '4:Run GOA/Human Load' | tee -a ${GOLOG}
+${GOLOAD}/goahuman/goahuman.sh | tee -a ${GOLOG}
 
-echo '5:Run GOA/CFP Load' | tee -a ${LOG}
-${GOLOAD}/goacfp/goacfp.sh | tee -a ${LOG}
+echo '5:Run GOA/CFP Load' | tee -a ${GOLOG}
+${GOLOAD}/goacfp/goacfp.sh | tee -a ${GOLOG}
 
-date | tee -a $LOG
+date | tee -a $GOLOG
