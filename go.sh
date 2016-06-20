7e3f114d4459bc83c308ec57fef846889b13ddf1
Lori Corbani <lori.corbani@jax.org>
2016-06-20 06:15:23 -0400

    covert to git

--- go.sh
+++ go.sh
@@ -23,22 +23,22 @@ touch $GOLOG
  
 date | tee -a $GOLOG
 
-#echo '1A:Run GOA/Mouse/Noctua Load' | tee -a ${GOLOG}
-#${GOLOAD}/goamousenoctua/goamousenoctua.sh | tee -a ${GOLOG}
+echo '1:Run GOA/Mouse/Noctua Load' | tee -a ${GOLOG}
+${GOLOAD}/goamousenoctua/goamousenoctua.sh | tee -a ${GOLOG}
 
-echo '1:Run GOA/Mouse Load' | tee -a ${GOLOG}
+echo '2:Run GOA/Mouse Load' | tee -a ${GOLOG}
 ${GOLOAD}/goamouse/goamouse.sh | tee -a ${GOLOG}
 
-echo '2:Run GOA/Rat Load' | tee -a ${GOLOG}
+echo '3:Run GOA/Rat Load' | tee -a ${GOLOG}
 ${GOLOAD}/goarat/goarat.sh | tee -a ${GOLOG}
 
-echo '3:Run GOA/PAINT Load' | tee -a ${GOLOG}
+echo '4:Run GOA/PAINT Load' | tee -a ${GOLOG}
 ${GOLOAD}/goarefgen/goarefgen.sh | tee -a ${GOLOG}
 
-echo '4:Run GOA/Human Load' | tee -a ${GOLOG}
+echo '5:Run GOA/Human Load' | tee -a ${GOLOG}
 ${GOLOAD}/goahuman/goahuman.sh | tee -a ${GOLOG}
 
-echo '5:Run GOA/CFP Load' | tee -a ${GOLOG}
+echo '6:Run GOA/CFP Load' | tee -a ${GOLOG}
 ${GOLOAD}/goacfp/goacfp.sh | tee -a ${GOLOG}
 
 date | tee -a $GOLOG
