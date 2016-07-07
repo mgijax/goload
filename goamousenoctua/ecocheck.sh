661cb2dc441324250f762c356cb69374dcbaeb49
Lori Corbani <lori.corbani@jax.org>
2016-07-07 12:19:07 -0400

    TR12345/GO-Noctua/GPAD/GPI

--- goamousenoctua/ecocheck.sh
+++ goamousenoctua/ecocheck.sh
@@ -14,35 +14,14 @@ touch $ECOLOG
  
 date | tee -a $ECOLOG
  
+echo 'Eco Id -> Evidence Code associations' | tee -a ${ECOLOG}
+ecolib.py | sort | tee -a ${ECOLOG}
+
+echo '' | tee -a ${ECOLOG}
+
+echo 'Eco Id -> Evidence Code associations used in GO annotatins' | tee -a ${ECOLOG}
+
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a ${ECOLOG}
-
-(
-select distinct a.accID as ecoID, s.synonym
-from ACC_Accession a, MGI_Synonym s, MGI_SynonymType st, VOC_Term t
-where a._LogicalDB_key = 182 
-and a._Object_key = s._Object_key
-and s._MGIType_key = 13
-and s.synonym = t.abbreviation
-and t._vocab_key = 3 
-and s._SynonymType_key = st._SynonymType_key
-and st.synonymtype = 'exact'
-union all 
-select distinct a2.accID, s.synonym
-from ACC_Accession a, MGI_Synonym s, MGI_SynonymType st, VOC_Term t, DAG_Closure dc, ACC_Accession a2
-where a._LogicalDB_key = 182 
-and a._Object_key = s._Object_key
-and s._MGIType_key = 13
-and s.synonym = t.abbreviation
-and t._vocab_key = 3 
-and s._SynonymType_key = st._SynonymType_key
-and st.synonymtype = 'exact'
-and a._Object_key = dc._ancestorobject_key
-and dc._descendentobject_key = a2._Object_key
-and a2._LogicalDB_key = 182 
-)
-order by synonym desc, ecoID
-;
-
 select distinct p.value, t.term, t.abbreviation, m.symbol
 from VOC_Annot a, VOC_Evidence e, VOC_Term t, MRK_Marker m, VOC_Evidence_Property p, VOC_Term pt
 where a._AnnotType_key = 1000
