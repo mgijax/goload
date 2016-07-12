39734e16a11409e0c4f2d4f858156b4ecccd88c5
Lori Corbani <lori.corbani@jax.org>
2016-07-12 13:47:07 -0400

    TR12349/TR12345/GO/GPAD/GPI

--- go.sh
+++ go.sh
@@ -42,6 +42,6 @@ echo '6:Run GOA/CFP Load' | tee -a ${GOLOG}
 ${GOLOAD}/goacfp/goacfp.sh | tee -a ${GOLOG}
 
 echo 'running go_annot_extensions_display_load...' | tee -a ${GOLOG}
-${MGICACHELOAD}/gxdexpression.csh | tee -a ${GOLOG}
+${MGICACHELOAD}/go_annot_extensions_display_load.csh | tee -a ${GOLOG}
 
 date | tee -a $GOLOG
