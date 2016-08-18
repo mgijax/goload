ee83e2d78557c8814413da2f83030bd4420740a7
Lori Corbani <lori.corbani@jax.org>
2016-08-18 08:35:45 -0400

    TR12345/GO-Noctua/GPAD/GPI

--- go.sh
+++ go.sh
@@ -43,7 +43,7 @@ echo '5:Run GOA/Human Load' | tee -a ${GOLOG}
 ${GOLOAD}/goahuman/goahuman.sh | tee -a ${GOLOG}
 
 echo '6:Run GOA/CFP Load' | tee -a ${GOLOG}
-${GOLOAD}/goacfp/goacfp.sh | tee -a ${GOLOG}
+${GOLOAD}/gocfp/gocfp.sh | tee -a ${GOLOG}
 
 echo 'running go_annot_extensions_display_load.csh' | tee -a ${GOLOG}
 ${MGICACHELOAD}/go_annot_extensions_display_load.csh | tee -a ${GOLOG}
