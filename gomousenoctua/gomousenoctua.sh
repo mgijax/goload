2e1c52da900423dd3aaf2c834d334cb8945180b6
Lori Corbani <lori.corbani@jax.org>
2022-04-07 10:58:18 -0400

    fixed link to /data/loads/go/goamouse/output/pubmed.error

--- gomousenoctua/gomousenoctua.sh
+++ gomousenoctua/gomousenoctua.sh
@@ -163,4 +163,4 @@ shutDown
 #
 cd ${OUTPUTDIR}
 rm -rf pubmed.error
-grep "Invalid Reference" gomousenoctua.error | cut -f2,3 -d":" | sort | uniq
+grep "Invalid Reference" ${INFILE_NAME_ERROR} | cut -f2,3 -d":" | sort | uniq > pubmed.error
