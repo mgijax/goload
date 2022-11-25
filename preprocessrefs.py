18e07fcb154f036e5d81e5e2f02fee8848d9ed18
Lori Corbani <lori.corbani@jax.org>
2022-11-25 07:05:51 -0500

    wts2-1054/preprocessrefs.py/change 1 long pumedid query to 1 per pubmedid

--- preprocessrefs.py
+++ preprocessrefs.py
@@ -52,31 +52,33 @@ for line in inFile.readlines():
         pubmedids.append(p)
 inFile.close()
 
-# select where jnumid is null ; includes relevance = keep and discard
-cmd = '''
-select _refs_key, _relevance_key, pubmedid
-from bib_citation_cache
-where pubmedid in (%s)
-and jnumid is null
-''' % ','.join(pubmedids)
+for i in pubmedids:
+    # select where jnumid is null ; includes relevance = keep and discard
+    cmd = '''
+    select _refs_key, _relevance_key, pubmedid
+    from bib_citation_cache
+    where pubmedid in (%s)
+    and jnumid is null
+    ''' % i
+    #''' % ','.join(pubmedids)
 
-relevanceSQL = ""
-jnumSQL = ""
+    relevanceSQL = ""
+    jnumSQL = ""
 
-results = db.sql(cmd, 'auto')
-for r in results:
-    refsKey = r['_refs_key']
-    relevanceKey = r['_relevance_key']
+    results = db.sql(cmd, 'auto')
+    for r in results:
+        refsKey = r['_refs_key']
+        relevanceKey = r['_relevance_key']
 
-    # if 'discard', then add 'keep'
-    if relevanceKey == 70594666:
-        print('setting relevance = keep for: ', r['pubmedid'])
-        relevanceSQL += "\nselect * from BIB_keepWFRelevance(%s,1000);" % (refsKey);
+        # if 'discard', then add 'keep'
+        if relevanceKey == 70594666:
+            print('setting relevance = keep for: ', r['pubmedid'])
+            relevanceSQL += "\nselect * from BIB_keepWFRelevance(%s,1000);" % (refsKey);
 
-    # always add jnum and update bib_citation_cache
-    print('adding jnum for: ', r['pubmedid'])
-    jnumSQL += "\nselect * from ACC_assignJ(1000,%s);" % (refsKey)
-    jnumSQL += "\nselect * from BIB_reloadCache (%s);" % (refsKey)
+        # always add jnum and update bib_citation_cache
+        print('adding jnum for: ', r['pubmedid'])
+        jnumSQL += "\nselect * from ACC_assignJ(1000,%s);" % (refsKey)
+        jnumSQL += "\nselect * from BIB_reloadCache (%s);" % (refsKey)
 
 if len(relevanceSQL) > 0:
        #print(relevanceSQL)
