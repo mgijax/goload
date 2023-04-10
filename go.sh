baa0b091f8bee8f39b7e6204d29b047f5da458f2
Lori Corbani <lori.corbani@jax.org>
2023-04-10 06:18:58 -0400

    wts2-1137/fl2-283/The directory where the Noctua annotations are located is changing

--- go.sh
+++ go.sh
@@ -23,52 +23,52 @@ touch ${GOLOG}
  
 date | tee -a $GOLOG
 
-echo 'runnning proisoformload...'
+echo '1:runnning proisoformload...'
 ${PROISOFORMLOAD}/bin/proisoform.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo 'generate GPI file (gomousenoctua depends on it)...'
+echo '2:generate GPI file (gomousenoctua depends on it)...'
 REPORTOUTPUTDIR=${PUBREPORTDIR}/output;export REPORTOUTPUTDIR
 ${PYTHON} ${PUBRPTS}/daily/GO_gpi.py | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '1:running GO/Mouse/Noctua Load' | tee -a ${GOLOG}
+echo '3:running GO/Mouse/Noctua Load' | tee -a ${GOLOG}
 ${GOLOAD}/gomousenoctua/gomousenoctua.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '2:running GOA/Mouse Load' | tee -a ${GOLOG}
+echo '4:running GOA/Mouse Load' | tee -a ${GOLOG}
 ${GOLOAD}/goamouse/goamouse.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '3:running GO/Rat Load' | tee -a ${GOLOG}
+echo '5:running GO/Rat Load' | tee -a ${GOLOG}
 ${GOLOAD}/gorat/gorat.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '4:running GO/PAINT Load' | tee -a ${GOLOG}
+echo '6:running GO/PAINT Load' | tee -a ${GOLOG}
 ${GOLOAD}/gorefgen/gorefgen.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '5:running GOA/Human Load' | tee -a ${GOLOG}
+echo '7:running GOA/Human Load' | tee -a ${GOLOG}
 ${GOLOAD}/goahuman/goahuman.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo '6:running GO/CFP Load' | tee -a ${GOLOG}
+echo '8:running GO/CFP Load' | tee -a ${GOLOG}
 ${GOLOAD}/gocfp/gocfp.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo 'processing protein complex go_qualifier/part_of' | tee -a ${GOLOG}
+echo '9:processing protein complex go_qualifier/part_of' | tee -a ${GOLOG}
 ${GOLOAD}/proteincomplex.sh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo 'running go_annot_extensions_display_load.csh' | tee -a ${GOLOG}
+echo '10:running go_annot_extensions_display_load.csh' | tee -a ${GOLOG}
 ${MGICACHELOAD}/go_annot_extensions_display_load.csh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo 'running go_isoforms_display_load.csh' | tee -a ${GOLOG}
+echo '11:running go_isoforms_display_load.csh' | tee -a ${GOLOG}
 ${MGICACHELOAD}/go_isoforms_display_load.csh | tee -a ${GOLOG} || exit 1
 
 date | tee -a $GOLOG
-echo 'running BIB_updateWFStatusGO' | tee -a ${GOLOG}
+echo '12:running BIB_updateWFStatusGO' | tee -a ${GOLOG}
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a ${GOLOG}
 select BIB_updateWFStatusGO();
 EOSQL
