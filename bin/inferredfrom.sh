8e232f646676bfbe7fb417aa601c0e3d36f453c9
Lori Corbani <lori.corbani@jax.org>
2023-11-28 13:30:33 -0500

    wts2-1155/GOC taking over GOA mouse, GOA human, etc.

--- bin/inferredfrom.sh
+++ bin/inferredfrom.sh
@@ -15,7 +15,6 @@ touch ${LOG}
 date | tee -a ${LOG}
 
 ${PYTHON} ${GOLOAD}/bin/inferredfrom.py -S${MGD_DBSERVER} -D${MGD_DBNAME} -U${MGD_DBUSER} -P${MGD_DBPASSWORDFILE} |& tee -a ${LOG}
-#${PG_DBUTILS_BCP} ${MGD_DBSERVER} ${MGD_DBNAME} ACC_Accession ${OUTPUTDIR} ACC_Accession.bcp "|" "\n" ${PG_DB_SCHEMA} |& tee -a ${LOG}
 
 date | tee -a ${LOG}
 
